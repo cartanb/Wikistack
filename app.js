fcf16756c03519f935bc3ed3f1ded6efc2bc3e94
Brian Cartan <cartanb@gmail.com>
2021-04-29 17:10:11 -0400

    Partner change - working on POST /wiki/

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const layout = require("./views/layout");
-const { db, User, Page } = require("./models");
-const wiki = require("./routes/wiki");
-const users = require("./routes/users");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const layout = require('./views/layout');
+const { db } = require('./models');
+const wiki = require('./routes/wiki');
+const users = require('./routes/users');
 
 const app = express();
 
 db.authenticate().then(() => {
-  console.log("connected to the database");
+  console.log('connected to the database');
 });
 
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: false }));
-app.use("/wiki", wiki);
-app.use("/users", users);
+app.use('/wiki', wiki);
+app.use('/users', users);
 
-app.get("/", (req, res, next) => {
-  res.redirect("/wiki");
+app.get('/', (req, res, next) => {
+  res.redirect('/wiki');
 });
 
 const init = async () => {
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,29 @@
-const router = require("express").Router();
-const addPage = require("../views/addPage");
+const router = require('express').Router();
+const { Page } = require('../models');
+const addPage = require('../views/addPage');
 
 // get /wiki
-router.get("/", (req, res, next) => {
-  res.send("whatever");
+router.get('/', (req, res, next) => {
+  res.send('whatever');
 });
 
 // post /wiki
-router.post("/", () => {});
+router.post('/', async (req, res, next) => {
+  try {
+    const page = await Page.create({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+});
 
 //get /wiki/add
-router.get("/add", (req, res, next) => {
+router.get('/add', (req, res, next) => {
   res.send(addPage());
 });
 

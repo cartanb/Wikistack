35c955d1f487bbac747ae187c3838f9af503b771
Brian Cartan <cartanb@gmail.com>
2021-04-29 19:30:37 -0400

    Add page functionality

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,15 +1,15 @@
-const router = require("express").Router();
-const { Page } = require("../models");
-const addPage = require("../views/addPage");
-const wikiPage = require("../views/wikipage");
+const router = require('express').Router();
+const { Page } = require('../models');
+const addPage = require('../views/addPage');
+const wikipage = require('../views/wikipage');
 
 // get /wiki
-router.get("/", (req, res, next) => {
-  res.send("whatever");
+router.get('/', (req, res, next) => {
+  res.send('whatever');
 });
 
 // post /wiki
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     const page = await Page.create({
       title: req.body.title,
@@ -17,23 +17,23 @@ router.post("/", async (req, res, next) => {
     });
 
     // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
-    res.redirect("/");
+    res.status(201).redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
 });
 
 //get /wiki/add
-router.get("/add", (req, res, next) => {
+router.get('/add', (req, res, next) => {
   res.send(addPage());
 });
 
-router.get("/:slug", async (req, res, next) => {
+router.get('/:slug', async (req, res, next) => {
   //   res.send(`hit dynamic route at ${req.params.slug}`);
   const findPage = await Page.findAll({
     where: { slug: req.params.slug },
   });
-  res.json(findPage);
+  res.send(wikipage(findPage[0]));
 });
 
 module.exports = router;
